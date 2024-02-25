96fbafe952e29b9f3f302f89467e2bbe6247c9d2
Shup75 <djslee75@gmail.com>
2024-02-25 05:28:01 -0800

    Initial commit

---
+++ src/main/java/net/shup/shuppybiomes/ShuppyBiomes.java
@@ -0,0 +1,16 @@
+package net.shup.shuppybiomes;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShuppyBiomes implements ModInitializer {
+	public static final String MOD_ID = "shuppy-biomes";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/shup/shuppybiomes/ShuppyBiomesClient.java
@@ -0,0 +1,10 @@
+package net.shup.shuppybiomes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShuppyBiomesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
